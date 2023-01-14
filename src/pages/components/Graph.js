56a95047d08e77deaf1f66259b3a869f3946edea
irohasuko <s18f1037@bene.fit.ac.jp>
2023-01-15 04:08:38 +0900

    グラフ範囲の調整

--- src/pages/components/Graph.js
+++ src/pages/components/Graph.js
@@ -25,7 +25,13 @@ const Graph = ({ population }) => {
   return (
     <ResponsiveContainer width="100%" height={400}>
       <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
-        <XAxis type="number" dataKey="year" unit="年" domain={['dataMin', 'dataMin + 50']} />
+        <XAxis
+          type="number"
+          dataKey="year"
+          unit="年"
+          domain={['dataMin', 2020]}
+          allowDataOverflow={true}
+        />
         <YAxis type="number" dataKey="value" name="総人口" unit="人" />
         <CartesianGrid strokeDasharray="3 3" />
         <Legend />
