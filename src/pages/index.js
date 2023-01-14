c78a810138e93b4b72fe5f97ec4ad058f63f2c7f
irohasuko <s18f1037@bene.fit.ac.jp>
2023-01-14 14:22:54 +0900

    indexにセレクター追加

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Header from './components/Header';
+import Selector from './components/Selector';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Selector />
     </>
   );
 }
