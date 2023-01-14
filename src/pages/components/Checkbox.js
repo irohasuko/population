1adb29e05a8db90c945f4d84c48a5a0ce207d077
irohasuko <s18f1037@bene.fit.ac.jp>
2023-01-15 04:56:09 +0900

    チェックボックスのcss調整

--- src/pages/components/Checkbox.js
+++ src/pages/components/Checkbox.js
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 const StyledCheck = styled.div`
   margin: 5px;
   border: none;
-  width: 15%;
-  @media screen and (min-width: 600px) {
-    width: 10%;
+  width: 30%;
+  @media screen and (min-width: 500px) {
+    width: 20%;
   }
-  @media screen and (min-width: 900px) {
-    width: 8%;
+  @media screen and (min-width: 800px) {
+    width: 12%;
   }
 `;
 
